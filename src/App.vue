cb45c04a72c87f66149c158839f6b8f8f55de84a
Lflores2022 <lflores@comercialmm.com>
2023-11-21 22:29:00 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <h1>Pagina con router</h1>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/gallery">Galeria</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(30, 40, 50, 0.8);
+  color: rgb(247, 246, 246);
+}
+
+nav a {
+  color: #fff;
+  margin-right: 5px;
+  font-size: 1.5rem;
+  text-decoration: none;
+}
+
+nav a:hover {
+  color: #111111;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,38 @@
+
+<template>
+  
+<div class="container">
+  <h1>#bienvenido <br> internauta</h1>
+  <hr/>
+</div>
+</template>
+
+<style scoped>
+.container {
+  background-color: rgba(30, 40, 50, 0.8);
+  height: 93.1vh;
+  padding-left: 10%;
+  position: relative;
+}
+
+h1 {
+  margin: 0;
+  line-height: 1;
+  font-size: 5em;
+  letter-spacing: -1px;
+  padding-top: 30vh;
+}
+
+p {
+  color: #DDD;
+}
+
+hr {
+  margin-top: 1rem;
+  height: 6px;
+  background-color: white;
+  width: 10rem;
+  margin-left: 0;
+}
+
+</style>
